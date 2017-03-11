9f1533fe0fc94c9a287c735a6e64b81d22349d1b
RGV\Ricardo <ricardo.guzman.velasco@outlook.com>
2017-03-11 12:32:18 +0100

    Añadidas clases de pieza y de barajador

---
+++ Touzzle/Assets/Scripts/Shuffler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shuffler : MonoBehaviour
+{
+    public List<Object> shapes;
+    public List<Object> pictures;
+   // public PieceTable board;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void shuffle()
+    {
+    }
+
+    void Awake()
+    {
+        shapes = new List<Object>(Resources.LoadAll("Shapes"));
+        pictures = new List<Object>(Resources.LoadAll("Pictures"));
+    }
+}
